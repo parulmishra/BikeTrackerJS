efe0c46fbb5a5bcdaaf5c9d152c1bc64ab8945b1
Guest User <Guest@epicodus-25.local>
2017-09-13 16:07:27 -0700

    not complete

--- js/bike-interface.js
+++ js/bike-interface.js
@@ -5,15 +5,16 @@ $(document).ready(function(){
   $("#search").submit(function(event){
 
     event.preventDefault();
-    let manufacturer = $("#manufacturer");
-    let distance = $("#distance");
-    
+    let manufacturer = $("#manufacturer").val();
+    let distance = $("#distance").val();
 
-    let newbike = new Bike(description,distance,location);
+
+    // let newbike = new Bike(manufacturer,distance);
     // var searchResult = newbike.getBikes();
     let promise = new Promise(function(resolve,reject){
     let xhr = new XMLHttpRequest();
-    let url = `https://bikeindex.org:443/api/v3/search/count?manufacturer=${manufacturer}&location=IP&distance=${distance}&stolenness=stolen&access_token=api_key`;
+    let url =
+    `https://bikeindex.org:443/api/v3/search?page=1&per_page=25&manufacturer=${manufacturer}&distance=${distance}&stolenness=stolen`;
     xhr.onload = function()
     {
       if(this.status == 200)
@@ -24,14 +25,18 @@ $(document).ready(function(){
       {
         reject(Error(xhr.statusText));
       }
-    };
-    request.open("GET", url, true);
-    request.send();
+    }
+    xhr.open("GET", url, true);
+    xhr.send();
     });
 
     promise.then(function(response) {
     let body = JSON.parse(response);
-     $('#bikes').text(`Stolen bikes are ${body}`);
+    for(var i=0; i<body.length; i++)
+    {
+      console.log(body[i]);
+    }
+    // $("#bikes").text(`your bikes + ${body}`);
    }, function(error) {
      $('.showErrors').text(`There was an error processing your request: ${error.message}`);
    });
--- js/bike.js
+++ js/bike.js
@@ -1,16 +1,16 @@
-export class Bike
-{
-  constructor(color,distance,location)
-  {
-    this.color = color;
-    this.distance = distance;
-    this.location = location;
-    this.serial = "";
-  }
+// export class Bike
+// {
+//   constructor(color,distance,location)
+//   {
+//     this.color = color;
+//     this.distance = distance;
+//     this.location = location;
+//     this.serial = "";
+//   }
   // function getBikes()
   // {
   //   let newBike = new Bike(color,distance,location);
   //   bikes.push(newBike);
   //   return bikes;
   // }
-}
+//}
